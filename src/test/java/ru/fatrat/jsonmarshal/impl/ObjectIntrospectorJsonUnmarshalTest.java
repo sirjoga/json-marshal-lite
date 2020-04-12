836ec3e291a8fe344e8dbb8707fcf74e2c21839c
Sergey Kolpakov <S.Kolpakov@x5.ru>
2020-04-12 22:53:37 +0300

    boolean introspector test

--- src/test/java/ru/fatrat/jsonmarshal/impl/ObjectIntrospectorJsonUnmarshalTest.java
+++ src/test/java/ru/fatrat/jsonmarshal/impl/ObjectIntrospectorJsonUnmarshalTest.java
@@ -60,18 +60,22 @@ public class ObjectIntrospectorJsonUnmarshalTest {
         private JsonOptional<String> opt2;
         @JsonOptionalClass(String.class) public JsonOptional<String> getOpt2() { return opt2; }
         public void setOpt2(JsonOptional<String> value) { opt2 = value; }
+        private boolean bool;
+        public boolean isBool() { return bool;}
+        public void setBool(boolean value) {  bool = value;}
     }
     
     @Test public void test() {
         Assertions.assertEquals((Integer) 1, subj.unmarshal(parse("1"), Integer.class));
         Assertions.assertEquals("a", subj.unmarshal(parse("\"a\""), String.class));
-        T t = subj.unmarshal(parse("{'a':1, 'b':['c','d'],'c':'EB','opt1':null,'opt2':'a'}".replace("'", "\"")), T.class);
+        T t = subj.unmarshal(parse("{'a':1, 'b':['c','d'],'c':'EB','opt1':null,'opt2':'a','bool':true}".replace("'", "\"")), T.class);
         Assertions.assertEquals(t.a, 1);
         Assertions.assertEquals(t.b.length, 2);
         Assertions.assertEquals(t.b[0], "c");
         Assertions.assertEquals(t.b[1], "d");
         Assertions.assertNull(t.opt1.value);
         Assertions.assertEquals(t.opt2.value, "a");
+        Assertions.assertTrue(t.bool);
     }
 
 }
