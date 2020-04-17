8c633c619b021d33ffd942ef16793902b5fb41ab
Sergey Kolpakov <S.Kolpakov@x5.ru>
2020-04-17 23:03:15 +0300

    Generic errors

--- src/main/java/ru/fatrat/jsonmarshal/JsonUnmarshal.java
+++ src/main/java/ru/fatrat/jsonmarshal/JsonUnmarshal.java
@@ -3,20 +3,29 @@ package ru.fatrat.jsonmarshal;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import javax.json.JsonValue;
+import java.util.Optional;
 
 public interface JsonUnmarshal {
 
     @Nullable <T> T unmarshal(
             @Nonnull JsonValue source,
-            @Nonnull Class<?> destClass,
+            @Nonnull Class<T> destClass,
             @Nullable JsonMarshalAnnotationSource annotationSource
     );
 
     @Nullable default <T> T unmarshal(
             @Nonnull JsonValue source,
-            @Nonnull Class<?> destClass
+            @Nonnull Class<T> destClass
     ) {
         return unmarshal(source, destClass, null);
+    }
+
+    @Nonnull default <T> T unmarshalNonNull(
+            @Nonnull JsonValue source,
+            @Nonnull Class<T> destClass
+    ) {
+        return Optional.ofNullable(unmarshal(source, destClass)).orElseThrow(() ->
+                new JsonMarshalException("Null unmarshal result"));
     }
 
 }
