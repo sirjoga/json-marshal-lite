718f027940c8c60bfd36500bc4bbad5ca1324ebd
Sergey Kolpakov <sirjoga@gmail.com>
2021-01-30 16:12:13 +0300

    more tests

--- src/test/java/ru/fatrat/jsonmarshal/methods/MethodAutoMarshalTest.java
+++ src/test/java/ru/fatrat/jsonmarshal/methods/MethodAutoMarshalTest.java
@@ -1,19 +1,16 @@
 package ru.fatrat.jsonmarshal.methods;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.junit.MockitoJUnitRunner;
 import org.mockito.junit.jupiter.MockitoExtension;
 import ru.fatrat.jsonmarshal.JsonGeneratorHelper;
 import ru.fatrat.jsonmarshal.JsonMarshal;
 import ru.fatrat.jsonmarshal.JsonMarshalAnnotationSource;
 import ru.fatrat.jsonmarshal.JsonUnmarshal;
-import ru.fatrat.jsonmarshal.impl.StandardJsonMarshal;
-import ru.fatrat.jsonmarshal.impl.StandardJsonUnmarshal;
 
+import javax.json.JsonValue;
 import java.lang.reflect.Method;
 import java.lang.reflect.Type;
 import java.util.Arrays;
@@ -41,27 +38,23 @@ class MethodAutoMarshalTest {
     @Mock
     private JsonUnmarshal unmarshal;
 
+    @InjectMocks
     private MethodAutoMarshal subj;
 
     public static abstract class TestObj {
         public abstract void noArgNoRes();
         public abstract int noArgIntRes();
+        public abstract void intArgNoRes(int a);
     }
 
-    @BeforeEach
-    public void beforeEach() {
-        JsonMarshal mockMarshal;
-        subj = new MethodAutoMarshal(marshal, unmarshal);
-    }
-
-    private static Method findMethod(Class<?> cls, String name) {
-        return Arrays.stream(cls.getMethods()).filter(m -> m.getName().equals(name)).findAny().orElseThrow(() ->
+    private static Method findMethod(String name) {
+        return Arrays.stream(TestObj.class.getMethods()).filter(m -> m.getName().equals(name)).findAny().orElseThrow(() ->
                 new IllegalArgumentException(String.format("No method: %s", name)));
     }
 
     @Test
     public void testNoArgRes() {
-        AutoMarshalInstance instance = subj.create(testObj, findMethod(TestObj.class, "noArgNoRes"));
+        AutoMarshalInstance instance = subj.create(testObj, findMethod("noArgNoRes"));
         assertFalse(instance.hasResult());
         assertFalse(instance.hasArgument());
         ResultMarshaller res = instance.process(null);
@@ -71,17 +64,30 @@ class MethodAutoMarshalTest {
 
     @Test
     public void testNoArgIntRes() {
-        AutoMarshalInstance instance = subj.create(testObj, findMethod(TestObj.class, "noArgIntRes"));
+        AutoMarshalInstance instance = subj.create(testObj, findMethod("noArgIntRes"));
         assertTrue(instance.hasResult());
         assertFalse(instance.hasArgument());
         when(testObj.noArgIntRes()).thenReturn(1);
         ResultMarshaller res = instance.process(null);
         verify(testObj, times(1)).noArgIntRes();
         Objects.requireNonNull(res).process(mock(JsonGeneratorHelper.class));
-        verify(marshal).marshal(any(int.class), any(Type.class), any(JsonGeneratorHelper.class));
+        verify(marshal).marshal(eq(1), any(Type.class), any(JsonGeneratorHelper.class));
         verifyNoMoreInteractions(marshal);
         verifyNoMoreInteractions(testObj);
-        verifyNoMoreInteractions(marshal);
+        verifyNoInteractions(unmarshal);
+    }
+
+    @Test
+    public void testIntArgNoRes() {
+        AutoMarshalInstance instance = subj.create(testObj, findMethod("intArgNoRes"));
+        assertFalse(instance.hasResult());
+        assertTrue(instance.hasArgument());
+        JsonValue arg = mock(JsonValue.class);
+        when(unmarshal.unmarshal(eq(arg), eq(int.class), any(JsonMarshalAnnotationSource.class))).thenReturn((int)1);
+        ResultMarshaller res = instance.process(() -> arg);
+        verify(testObj, times(1)).intArgNoRes(eq(1));
+        verifyNoMoreInteractions(testObj);
+        verifyNoInteractions(marshal);
     }
 
 }
